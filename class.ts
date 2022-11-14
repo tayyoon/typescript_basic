bbcedca781c84f7da04d12859f5b3c6f0c84d942
tay yoon <dylanyoon92@gmail.com>
2022-11-14 23:57:05 +0900

    class 왜 사라졌어

--- class.ts
+++ class.ts
@@ -4,4 +4,15 @@ abstract class Car {
   constructor(color: string) {
     this.color = color;
   }
+  start() {
+    console.log('start');
+  }
 }
+
+class Bmw2 extends Car {
+  constructor(color: string) {
+    super(color);
+  }
+}
+
+const z5 = new Bmw2('black');
