af1e892426c13a2bb3f9c7e7708dde860b83b6db
tay yoon <dylanyoon92@gmail.com>
2022-11-15 00:01:54 +0900

    클래스 어디갔냐..

--- class.ts
+++ class.ts
@@ -1,5 +1,6 @@
-// 추상 class abstract
-abstract class Car {
+// 추상 class abstract : 추상클래스는  new를 통해서 객체를 만들 수 없다. 상속을 통해서만 가능하다.
+
+abstract class Car4 {
   color: string;
   constructor(color: string) {
     this.color = color;
@@ -7,11 +8,18 @@ abstract class Car {
   start() {
     console.log('start');
   }
+  abstract doSomething(): void;
 }
 
-class Bmw2 extends Car {
+// const car = new Car('red')
+
+class Bmw2 extends Car4 {
   constructor(color: string) {
     super(color);
+  }
+  // 추상클래스 내부의 추상매서드는 반드시 상속받은쪽에서 구체적인 구현을 해줘야한다.
+  doSomething() {
+    alert(3);
   }
 }
 
